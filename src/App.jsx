1d0300f61d049e4f4efc506b809caadee8a630c8
Buringskul <simonaisakova6@gmail.com>
2024-09-19 13:03:28 -0400

    Solved

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,14 @@ function App() {
 
   let card;
 
+  function NextQuestion() {
+    const url = "https://opentdb.com/api.php?amount=1&category=9&type=multiple"
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => setQuestionData(data.results[0]))
+
+  }
+
   if (selectedAnswer) {
     card = (
       <ResultCard
@@ -41,7 +49,7 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button className="btn btn-success" onClick={NextQuestion}>Next Question</button>
         {card}
       </div>
     </div>
